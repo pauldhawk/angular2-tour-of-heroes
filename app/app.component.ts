50f164832ea79a3a126b2e7cf7510f81f2862bdb
Kazuhiko Hotta <kazuhiko.hotta@gmail.com>
2016-07-04 15:46:10 +0900

    for a routing test

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService }         from './hero.service';
+import {Component} from '@angular/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {DashboardComponent}  from './dashboard.component';
+import {HeroesComponent}     from './heroes.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroService}         from './hero.service';
 @Component({
-  selector: 'my-app',
-  template: `
+    selector:   'my-app',
+    template:   `
     <h1>{{title}}</h1>
     <nav>
       <a [routerLink]="['Dashboard']">Dashboard</a>
@@ -14,19 +14,18 @@ import { HeroService }         from './hero.service';
     </nav>
     <router-outlet></router-outlet>
   `,
-  styleUrls: ['app/app.component.css'],
-  directives: [ROUTER_DIRECTIVES],
-  providers: [
-    ROUTER_PROVIDERS,
-    HeroService,
-  ]
+    styleUrls:  ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers:  [
+        ROUTER_PROVIDERS,
+        HeroService,
+    ]
 })
-//noinspection TypeScriptValidateTypes
 @RouteConfig([
-  { path: '/dashboard',  name: 'Dashboard',  component: DashboardComponent, useAsDefault: true },
-  { path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent },
-  { path: '/heroes',     name: 'Heroes',     component: HeroesComponent }
+    {path: '', name: 'Dashboard', component: DashboardComponent, useAsDefault: true},
+    {path: '/detail/:id', name: 'HeroDetail', component: HeroDetailComponent},
+    {path: '/heroes', name: 'Heroes', component: HeroesComponent}
 ])
 export class AppComponent {
-  title = 'Tour of Heroes';
+    title = 'Tour of Heroes';
 }
